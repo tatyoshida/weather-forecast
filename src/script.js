3b30b0fb5eb9f882b7f5d766bbf09687ba1f38fc
Tatiana Yoshida <tatyoshida@gmail.com>
2023-12-21 11:14:42 -0300

    forecastHtml function updated

--- src/script.js
+++ src/script.js
@@ -58,9 +58,29 @@ function formatDate(date) {
   return `${formatDay} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function(day){
+    forecastHtml = forecastHtml + `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png" class="forecast-icon">
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">8°</div>
+          <div class="weather-forecast-temperature"><strong>10°</strong></div>
+        </div>
+      </div>
+    `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", searchSubmit);
 
 searchCity("Montevideo");
-
-
+displayForecast();
